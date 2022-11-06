72a52757be8499ec8d295f58b30413e3e907b0f5
UHassam0 <umarhassam786@gmail.com>
2022-11-06 18:35:36 +0000

    Set box 1 to fill with O when clicked, ready to build game flows round this idea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log('script connected please')
+//document.onload = alert('Are you ready to play?');
+
+
+let box1 = document.getElementById('1');
+let box2 = document.getElementById('2');
+let box3 = document.getElementById('3');
+let box4 = document.getElementById('4');
+let box5 = document.getElementById('5');
+let box6 = document.getElementById('6');
+let box7 = document.getElementById('7');
+let box8 = document.getElementById('8');
+let box9 = document.getElementById('9');
+
+let grid = document.getElementsByClassName('grid-boxes');
+//let boxNumber = grid[i]
+
+function insertNought(event) {
+    let box1 = document.getElementById('1');
+    box1.innerHTML = 'O'
+}
+
+box1.addEventListener('click', insertNought)
+
+
+
