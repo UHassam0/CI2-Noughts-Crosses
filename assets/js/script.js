d0cbb01e6cc333bff3cd343bdd607f5e2c130cd4
UHassam0 <umarhassam786@gmail.com>
2022-11-09 19:59:44 +0000

    Create if statements to evaluate wins

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,44 @@
-//document.onload = alert('Are you ready to play?');
-let nought = document.getElementById('nought');
-let cross = document.getElementById('cross');
+let box1 = document.getElementById('1').innerHTML;
+let box2 = document.getElementById('2').innerHTML;
+let box3 = document.getElementById('3').innerHTML;
+let box4 = document.getElementById('4').innerHTML;
+let box5 = document.getElementById('5').innerHTML;
+let box6 = document.getElementById('6').innerHTML;
+let box7 = document.getElementById('7').innerHTML;
+let box8 = document.getElementById('8').innerHTML;
+let box9 = document.getElementById('9').innerHTML;
 
-nought.addEventListener('click', playAsNought);
 
-function playAsNought(event) {
-    let grid = document.getElementsByClassName('grid-boxes');
-    switch(grid[1].innerhtml) {
-    case  'O':
-    alert('You Win!');
-    
+
+function caseWin1() {
+    if (
+        box1 === box2 && box1 === box3
+        ||
+        box1 === box5 && box1 === box9
+        ||
+        box1 === box4 && box1 === box7
+    ) {
+       return `${box1} is the winner`;
+    }
+}
+function caseWin5() {
+    if (
+        box5 === box3 && box5 === box7
+        ||
+        box5 === box4 && box1 === box6
+    ) {
+       return `${box5} is the winner`;
     }
 }
 
-let box1 = document.getElementById('1');
-let box2 = document.getElementById('2');
-let box3 = document.getElementById('3');
-let box4 = document.getElementById('4');
-let box5 = document.getElementById('5');
-let box6 = document.getElementById('6');
-let box7 = document.getElementById('7');
-let box8 = document.getElementById('8');
-let box9 = document.getElementById('9');
-
-
-
-
-function insertNought(event) {
-    let box1 = document.getElementById('1');
-    box1.innerHTML = 'O'
+function caseWin1() {
+    if (
+        box9 === box8 && box9 === box7
+        ||
+        box9 === box6 && box9 === box3
+    ) {
+       return `${box9} is the winner`;
+    }
 }
 
-box1.addEventListener('click', insertNought)
-
-
-
+console.log(caseWin1());
